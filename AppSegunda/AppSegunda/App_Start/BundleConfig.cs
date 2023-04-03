cf8ce44dac8f8dcbd695edd99baaf8d6e2e13603
yasmimx <101806624+yasmimx@users.noreply.github.com>
2023-04-03 09:04:29 -0300

    Finalizacao

--- AppSegunda/AppSegunda/App_Start/BundleConfig.cs
+++ AppSegunda/AppSegunda/App_Start/BundleConfig.cs
@@ -7,7 +7,11 @@ namespace AppSegunda
     {
         // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
+
         {
+            BundleTable.EnableOptimizations = true;
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
---
+++ AppSegunda/AppSegunda/Areas/Aluno/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppSegunda.Areas.Aluno.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Aluno/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
