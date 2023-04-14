adcd2f91dace58d2386a07cd203fd340b319f121
yasmimx <101806624+yasmimx@users.noreply.github.com>
2023-04-14 11:34:26 -0300

    CodPri

---
+++ AppTerceira/AppTerceira/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppTerceira.MvcApplication" Language="C#" %>
---
+++ AppTerceira/AppTerceira/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppTerceira.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public int Sexo { get; set;}
+
+        public Pessoa SelectPessoa()
+        {
+            Pessoa ObjPEssoa = new Pessoa();
+            return ObjPEssoa;
+        }
+    }
+}
